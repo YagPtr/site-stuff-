662fff4156a3c1660cdfb733111a489f4bf5d966
Аристархов <criper294@gmail.com>
2024-09-09 00:47:22 +0300

    Update LMAO.js

--- jscodes/LMAO.js
+++ jscodes/LMAO.js
@@ -3,6 +3,7 @@
 var myResetButton = document.querySelector("#nocat")
 
 myResetButton.onclick = function () {
-    alert("dead") 
     localStorage.setItem("bodyPressed", "dead")
+    alert("dead") 
+    
 }
