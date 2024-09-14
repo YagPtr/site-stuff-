f412c24a5fe23194b6eb4651d01839fb830b1a40
YagP <criper294@gmail.com>
2024-09-14 21:10:34 +0300

    та хз че я тут понадобавлял

--- jscodes/numb-guess.js
+++ jscodes/numb-guess.js
@@ -12,25 +12,32 @@ var guessField = document.querySelector('.guessField');
 var resetButton = document.querySelector('.resetButton');
 var guessCount = 1;
 
-
+let count=0;
 
 guessSubmit.addEventListener("click", checkGuess);
 resetButton.addEventListener("click", reset);
 function checkGuess() {
     guesses.textContent+=" "+guessField.value
-
+    count++;
     if (guessField.value>randomNumber){
         lowOrHi.textContent="твое число больше"
         guesses.textContent+="↓"
     }
     else if(guessField.value<randomNumber){
         lowOrHi.textContent="твое число меньше"
+        lowOrHi.textContent.toLowerCase
         guesses.textContent+="↑"
+        
     }
-    
+    for (let i=0;i<=count;i++)
+    {
+        var msg = document.createElement("p");
+        msg.textContent = "This is a message box";
+        lowOrHi.appendChild(msg);
+    }
     if (guessField.value==randomNumber)
         won();
-
+    
 }
 
 function won(){
@@ -44,3 +51,22 @@ function reset(){
     randomNumber = Math.floor(Math.random() * 100) + 1;
     resetButton.hidden=true
 }
+
+function whatever(){
+    for (var i = 1; i < 21; i++) {
+        console.log(i);
+      }
+}
+
+const btn = document.querySelector("button");
+
+function random(number) {
+  return Math.floor(Math.random() * (number + 1));
+}
+
+btn.onclick = function () {
+  const rndCol =
+    "rgb(" + random(255) + "," + random(255) + "," + random(255) + ")";
+  document.body.style.backgroundColor = rndCol;
+};
+
